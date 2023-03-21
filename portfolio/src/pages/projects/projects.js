d33965d99447a55e20d88ed1cfe6735bcb2c4026
Guillaume Robert <guillaumerober@gmail.com>
2023-03-21 16:54:17 +0100

    projects page ok

--- portfolio/src/pages/projects/projects.js
+++ portfolio/src/pages/projects/projects.js
@@ -7,18 +7,22 @@ function Projects() {
   return (
     
     <div className="projects_area">
+      <div className="projects_container">
       {projectsData.map((p) => (
         <Link key={p.id} to={`/projet/${p.id}`}>
+          
           <div
             className="project_card"
             style={{
-              backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%), url(${p.cover}) `,
+             backgroundImage: `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 100%), url(${p.cover})`,
+
             }}
           >
             <p className="project_card_title">{p.title}</p>
           </div>
         </Link>
       ))}
+    </div>
     </div>
   );
 }
